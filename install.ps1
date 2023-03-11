eaa3704ea94c8f873a2ece35233477c20d1b9e5a
Victor Santana <vsantana@microsoft.com>
2023-03-11 01:24:07 -0600

    Fix source agreement and exit bug

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 # Re-Execute the script in admin mode in case it's not already
 # Required to install windows fonts
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-ExecutionPolicy Unrestricted -c `"iex (iwr https://raw.githubusercontent.com/Welasco/PowerLine/master/install.ps1)`" " -Verb RunAs; exit}
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-ExecutionPolicy Unrestricted -c `"iex (iwr https://raw.githubusercontent.com/Welasco/PowerLine/master/install.ps1)`" " -Verb RunAs; exit }
 # Download Windows CacadiaCode Font
 $releases = Invoke-RestMethod -Uri https://api.github.com/repos/ryanoasis/nerd-fonts/releases
 $cascadia_release = ($releases[0].assets | Where{$_.Name -eq "CascadiaCode.zip"}).browser_download_url
