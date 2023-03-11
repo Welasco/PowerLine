27b5397d9164d89df346f79725eaaa553ccb3a99
Victor Santana <vsantana@microsoft.com>
2023-03-11 01:03:39 -0600

    test file

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
 #if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-ExecutionPolicy Unrestricted -File `"$PSCommandPath`"" -Verb RunAs; exit }
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-ExecutionPolicy Unrestricted -c `"iex (iwr https://raw.githubusercontent.com/Welasco/PowerLine/master/test.ps1)`" " -Verb RunAs; exit }
 Get-ChildItem c:\
+Read-Host
