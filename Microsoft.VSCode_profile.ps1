4834ccafe1d928a3e51fda898e79a8e23bb94d0e
Victor Santana <26201510+Welasco@users.noreply.github.com>
2023-08-31 14:55:49 -0500

    Update Microsoft.VSCode_profile.ps1

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -1,1 +1,3 @@
+# Need to be tested but with Onedrive that's looks like to be the best solution using a special folder
+#.(([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments))+"\PowerShell\Microsoft.PowerShell_profile.ps1")
 .$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
